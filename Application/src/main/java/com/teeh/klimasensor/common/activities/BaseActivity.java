1888b630d5b1ac53969bedffbf82103db865ba54
teehtheg <david.tschirky@gmail.com>
2017-11-13 18:59:02 +0100

    update

--- Application/src/main/java/com/teeh/klimasensor/common/activities/BaseActivity.java
+++ Application/src/main/java/com/teeh/klimasensor/common/activities/BaseActivity.java
@@ -17,14 +17,7 @@
 package com.teeh.klimasensor.common.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.FrameLayout;
-
-import com.teeh.klimasensor.R;
 
 /**
  * Base launcher activity, to handle most of the common plumbing for samples.
